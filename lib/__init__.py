268dac2c182c756f4d1181c88c22040fd23462b0
Eugene R. Miller <theerm@gmail.com>
2014-03-01 13:23:09 -0700

    fixed max truescore bug

--- lib/__init__.py
+++ lib/__init__.py
@@ -106,7 +106,7 @@ DEFAULT_PERCENT_PLAYED = cfg.get('Defaults', 'percent_played', 50.0, float)
 DEFAULT_TRUE_SCORE = (((DEFAULT_RATING * 2 * 10.0) + (DEFAULT_SCORE * 10.0) + 
                         DEFAULT_PERCENT_PLAYED) / 3)
 
-MAX_TRUE_SCORE = (((6 * 2 * 10.0) + (6 * 10.0) + 
+MAX_TRUE_SCORE = (((6 * 2 * 10.0) + (10 * 10.0) + 
                     100.0) / 3)
 
 # print "IMPORTED __init__"
